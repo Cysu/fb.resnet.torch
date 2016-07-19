81b74b834bb421629c65a4e7819fd4e6ab4c8e9f
Cysu <st.cysu@gmail.com>
2016-07-19 14:14:01 +0800

    Update default options

--- train.lua
+++ train.lua
@@ -165,12 +165,14 @@ end
 function Trainer:learningRate(epoch)
    -- Training schedule
    local decay = 0
+   local ratio = 0.1
    if self.opt.dataset == 'imagenet' then
       decay = math.floor((epoch - 1) / 30)
    elseif self.opt.dataset == 'cifar10' then
       decay = epoch >= 160 and 3 or epoch >= 120 and 2 or epoch >= 60 and 1 or 0
+      ratio = 0.2
    end
-   return self.opt.LR * math.pow(0.1, decay)
+   return self.opt.LR * math.pow(ratio, decay)
 end
 
 return M.Trainer
